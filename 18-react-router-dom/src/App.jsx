70e986c61de8157bb87e4778789786b059626f5f
Pratyutpana <pratyutpanaray@gmail.com>
2026-07-18 21:12:33 +0530

    added product link

--- 18-react-router-dom/src/App.jsx
+++ 18-react-router-dom/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/Contact'
 import Navbar from './components/Navbar'
+import Product from './pages/Product'
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Route path='/' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
         <Route path='/contact' element={<Contact/>}/>
+        <Route path='/product' element={<Product/>}/>
       </Routes>
       
     </div>
--- 18-react-router-dom/src/components/Navbar.jsx
+++ 18-react-router-dom/src/components/Navbar.jsx
@@ -9,6 +9,7 @@ const Navbar = () => {
         <Link to ='/'>Home</Link>
         <Link to ='/about'> About</Link>
         <Link to ='/contact'>Contact</Link>
+        <Link to ='/product'>Product</Link>
        </div>
     </div>
   )
