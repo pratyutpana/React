9325f2f6301cafdc00ec9da0bfe38c13c3515590
Pratyutpana <pratyutpanaray@gmail.com>
2026-07-20 10:18:37 +0530

    20-bonus-concept

---
+++ 20-Bonus-concepts/src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import Navbar from './components/Navbar'
+
+const App = () => {
+
+  const [Theme , setTheme] = useState('light')
+
+  return (
+    <div>
+      <h1>Theme is {Theme}</h1>
+      <Navbar Theme={Theme} setTheme={setTheme}/>
+    </div>
+  )
+}
+
+export default App
---
+++ 20-Bonus-concepts/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = (props) => {
+
+    console.log(props)
+
+  return (
+    <div>
+      <button onClick={()=>{
+        props.setTheme('Dark')
+      }}>
+        Change Theme
+        </button>
+    </div>
+  )
+}
+
+export default Navbar
